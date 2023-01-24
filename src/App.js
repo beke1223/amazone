e02245b0e950deba7903f611dee8935bbf5c6f45
beke1223 <101667987+beke1223@users.noreply.github.com>
2023-01-24 20:57:55 +0300

    creating react app

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="#Home">amazon</a>
+      </header>
+      <main></main>
+    </div>
+  );
+}
+
+export default App;
